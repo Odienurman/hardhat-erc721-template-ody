cc4feff7c2b44841b779fdfcf109fe2facd96020
Odie Nurman <88243064+Odienurman@users.noreply.github.com>
2025-01-27 18:57:14 +0700

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,9 +4,9 @@ export async function main() {
     console.log('Deploying AnyFlowERC721...');
 
     const args = [
-        '0xA71a71A71a71a71A71a71a71a71A71a71A71A71a',   // _initialOwner
-        'AnyFlowERC721', // _name
-        'AF',           // _symbol
+        '0x2263a1C2084EE61249dAE93876A396Ac8D20CD4B',   // _initialOwner
+        'anyflow monkey', // _name
+        'ANYM',           // _symbol
     ] as const;
 
     const contract = await hre.viem.deployContract("AnyFlowERC721", args);
